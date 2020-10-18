e2fcfcf3c3a6ecbc8dd30b5e16fba43027334e89
Özkan Baklacı <ozkan.baklaci@demirorenteknoloji.com>
2020-10-18 23:56:46 +0300

    Transfer Application Data Domain

---
+++ MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+
+namespace MicroRabbit.Transfer.Data.Repository
+{
+    public class TransferRepository : ITransferRepository
+    {
+        private readonly TransferDbContext _transferDbContext;
+
+        public TransferRepository(TransferDbContext transferDbContext)
+        {
+            _transferDbContext = transferDbContext;
+        }
+
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _transferDbContext.TransferLogs;
+        }
+    }
+}
