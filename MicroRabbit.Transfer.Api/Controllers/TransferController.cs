498e321815c4b4ff636bbfd96f54286de6b0f7f1
Özkan Baklacı <ozkan.baklaci@demirorenteknoloji.com>
2020-10-18 23:39:05 +0300

    DbContext and Domain Model

---
+++ MicroRabbit.Transfer.Api/Controllers/TransferController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.Transfer.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransferController : ControllerBase
+    {
+     
+    }
+}
---
+++ MicroRabbit.Transfer.Domain/Models/TransferLog.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroRabbit.Transfer.Domain.Models
+{
+    public class TransferLog
+    {
+        public int Id { get; set; }
+        public string FromAccount { get; set; }
+        public decimal ToAccount { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
